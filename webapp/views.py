d72f63f45a0fadde93a67d16ec93c992b31c5e8c
Conor Brady <conorpbrady@gmail.com>
2022-02-06 11:01:22 -0500

    enforce auth on pages

--- webapp/views.py
+++ webapp/views.py
@@ -1,39 +1,32 @@
 from django.shortcuts import render
 from django.views import generic
 from v1.models import Weight, Exercise, Lift
+from django.contrib.auth.mixins import LoginRequiredMixin
+
+
 # Create your views here.
 
-class IndexView(generic.ListView):
+class IndexView(generic.base.TemplateView):
     template_name = 'index.html'
-    context_object_name = 'some_name'
 
-    def queryset(self):
-        return Weight.objects.all()
-
-    def get_context_data(self, **kwargs):
-        context = super(IndexView, self).get_context_data(**kwargs)
-        context['weight_listing'] = self.queryset()
-        # context['lift_listing'] = Lift.objects.all()
-        return context
-
-class WeightView(generic.ListView):
+class WeightView(LoginRequiredMixin, generic.ListView):
     template_name = 'weight.html'
     context_object_name = 'weight'
 
     def queryset(self):
-        return Weight.objects.all()
+        return Weight.objects.filter(owner=self.request.user)
 
-class ExerciseView(generic.ListView):
+class ExerciseView(LoginRequiredMixin, generic.ListView):
     template_name = 'exercise.html'
     context_object_name = 'exercise'
 
     def queryset(self):
-        return Exercise.objects.all()
+        return Exercise.objects.filter(owner=self.request.user)
 
-class LiftsView(generic.ListView):
+class LiftsView(LoginRequiredMixin, generic.ListView):
     template_name = 'lifts.html'
     context_object_name = 'lifts'
 
     def queryset(self):
-        return Lift.objects.all()
+        return Lift.objects.filter(owner=self.request.user)
 
